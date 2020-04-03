b2d5635f172919b6cdd0c11bd163d39290e083d3
adrian1311 <azarquiel.103905@gmail.com>
2020-04-03 15:57:49 +0300

    Changes

--- src/main/java/com/steps/steps/dao/UserEntityRepository.java
+++ src/main/java/com/steps/steps/dao/UserEntityRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserEntityRepository extends CrudRepository<UserEntity, Long> {
+public interface UserEntityRepository extends CrudRepository<UserEntity, String> {
 
     @Query(value = "SELECT * FROM users WHERE username = :username", nativeQuery = true)
     UserEntity findByUsername(@Param("username") String username);
